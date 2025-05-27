409eb2f420d656f46c797aa72fb55bf3bce77f28
Thomas Bereknyei <tomberek@gmail.com>
2025-05-27 01:05:16 -0400

    fix: preBuild was used to fetch the vendor

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
           src = ./src;
           vendorHash = "sha256-5xR9WCkpPpY9D0LR2mcdoOX34RqVpxJjgRwc4GEkGiE=";
           outputs = ["out" "incremental"];
-          preBuild = ''
+          postConfigure = ''
             mkdir empty
             cp -r ${cache.packages.${system}.thing.incremental or "empty"} $incremental
             chmod -R +w $incremental
