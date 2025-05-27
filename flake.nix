2684bcc43fd77e9959fd73afa9e8ae10cc8185d0
Thomas Bereknyei <tomberek@gmail.com>
2025-05-27 01:49:17 -0400

    feat: add nginx-ccache

--- flake.nix
+++ flake.nix
@@ -7,6 +7,29 @@
     { self, nixpkgs, cache, ... }:
     {
       packages = builtins.mapAttrs (system: pkgs: rec {
+        hello-ccache = (pkgs.hello.override {stdenv = pkgs.ccacheStdenv;}).overrideAttrs (old: {
+          outputs = (old.outputs or ["out"]) ++ [ "incremental" ];
+          configureFlags = (old.configureFlags or []) ++ [
+	    "--cache-file=${placeholder "incremental"}/config.cache"
+	  ];
+          postPatch = ''
+            export CCACHE_COMPRESS=1
+            export CCACHE_DIR="$incremental"
+            export CCACHE_UMASK=007
+            export CCACHE_SLOPPINESS="random_seed"
+            export CCACHE_NOINODECACHE=1
+
+            mkdir empty
+            cp -r ${cache.packages.${system}.hello-ccache.incremental or "empty"} $incremental
+            chmod -R +w $incremental
+          '';
+          postInstall = (old.postInstall or "") + (if cache?packages then ''
+            ${pkgs.ccache}/bin/ccache --dir "$incremental" --show-stats
+          '' else ''
+            ${pkgs.ccache}/bin/ccache --dir "$incremental" --zero-stats --show-stats
+          '');
+        });
+
         golang = pkgs.buildGoModule {
           name = "golang";
           src = pkgs.lib.cleanSource ./golang;
